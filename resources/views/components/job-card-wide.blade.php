c123976faa87286e855c6a038d7e0d74a4009e4f
Prosanto bhakta <proshantbhakta@gmail.com>
2025-01-12 18:28:00 +0600

    Update job-card-wide.blade.php
    12th jan 6:30 pm

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -5,10 +5,10 @@
     </div>
 
 
-        <div class="flex-1">
-            <a href="#" class="self-start text-sm">Employer</a>
-            <h3> PHP programmer </h3>
-            <p> Full Time From-$60000 </p>
+        <div class="flex-1 flex flex-col">
+            <a href="#" class="self-start text-gray-400 text-sm">Employer</a>
+            <h3 class="font-bold text-xl mt-3"> PHP programmer </h3>
+            <p class="text-sm text-gray-400 mt-auto"> Full Time From-$60000 </p>
         </div>
         <div>
             <x-tag> tag1 </x-tag>
